8ac94953e31d0444ba61a4bd89189ac45f79ac33
Craig Bomba <talk2cbo@gmail.com>
2018-09-03 13:37:08 -0500

    update with heroku JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "trilogy",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDBURL) {
+  connection = mysql.createConnection(process.env.JAWSDBURL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "trilogy",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
